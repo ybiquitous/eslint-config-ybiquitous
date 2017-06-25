7bd47684811a63551af9236003b3669a2d2ddf4f
ybiquitous <ybiquitous@gmail.com>
2017-06-25 16:06:44 +0900

    feat: add `eslint-plugin-eslint-comments`

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = {
   extends: ['airbnb'],
-  plugins: ['compat'],
+  plugins: ['compat', 'eslint-comments'],
   rules: {
     semi: ['error', 'never'],
     'compat/compat': 'error',
+    'eslint-comments/disable-enable-pair': 'error',
+    'eslint-comments/no-duplicate-disable': 'error',
+    'eslint-comments/no-unlimited-disable': 'error',
+    'eslint-comments/no-unused-disable': 'error',
+    'eslint-comments/no-unused-enable': 'error',
+    'eslint-comments/no-use': 'error',
   },
   env: {
     browser: true,
