426c6101aa8b2b4ca774acec537d74740531724d
ybiquitous <ybiquitous@gmail.com>
2017-08-07 15:42:19 +0900

    refactor: lowercase shell script variables

--- test.sh
+++ test.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 set -eu
 
-WORKDIR=tmp
+workdir=tmp
 
 # setup
 tarball=$(npm pack)
-rm -rf $WORKDIR
-mkdir $WORKDIR
-cd $WORKDIR
+rm -rf $workdir
+mkdir $workdir
+cd $workdir
 mv "../${tarball}" .
 npm init -y
 export PATH="$(npm bin):${PATH}"
-NPM_OPTS='--no-progress --save-dev --prefer-offline'
+npm_opts='--no-progress --save-dev --prefer-offline'
 
 # test local
-npm install $NPM_OPTS eslint "file:./${tarball}"
+npm install $npm_opts eslint "file:./${tarball}"
 echo 'process.stdout.write(1)' > test.js
 echo '{"extends":"ybiquitous/base"}' > .eslintrc && eslint .
 echo '{"extends":"ybiquitous"}' > .eslintrc && eslint .
 
 # test remote
-npm install $NPM_OPTS "github:ybiquitous/eslint-config-ybiquitous"
+npm install $npm_opts "github:ybiquitous/eslint-config-ybiquitous"
 eslint .
 
 # teardown
 cd ..
-rm -rf $WORKDIR
+rm -rf $workdir
