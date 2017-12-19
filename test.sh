1084eb2939ac690440b8154b0dcb1de98b3f07f3
ybiquitous <ybiquitous@gmail.com>
2017-12-20 00:59:25 +0900

    test: refactor via ShellCheck
    https://github.com/koalaman/shellcheck

--- test.sh
+++ test.sh
@@ -3,26 +3,28 @@ set -eu
 
 workdir=tmp
 
-# setup
-tarball=$(npm pack)
-rm -rf $workdir
-mkdir $workdir
-cd $workdir
-mv "../${tarball}" .
-npm init -y
-export PATH="$(npm bin):${PATH}"
-npm_opts='--no-progress --save-dev'
+run () {
+  # setup
+  tarball=$(npm pack)
+  rm -rf $workdir
+  mkdir $workdir
+  cd $workdir
+  mv "../${tarball}" .
+  npm init -y
+  PATH="$(npm bin):${PATH}"
+  export PATH
+  npm_opts='--no-progress --save-dev'
 
-# test local
-npm install $npm_opts eslint "file:./${tarball}"
-echo 'process.stdout.write(1)' > test.js
-echo '{"extends":"ybiquitous/base"}' > .eslintrc && eslint .
-echo '{"extends":"ybiquitous"}' > .eslintrc && eslint .
+  # test local
+  npm install "$npm_opts" eslint "file:./${tarball}"
+  echo 'process.stdout.write(1)' > test.js
+  echo '{"extends":"ybiquitous/base"}' > .eslintrc && eslint .
+  echo '{"extends":"ybiquitous"}' > .eslintrc && eslint .
 
-# test remote
-npm install $npm_opts "github:ybiquitous/eslint-config-ybiquitous"
-eslint .
+  # test remote
+  npm install "$npm_opts" "github:ybiquitous/eslint-config-ybiquitous"
+  eslint .
+}
 
 # teardown
-cd ..
-rm -rf $workdir
+(run; rm -rf $workdir)
