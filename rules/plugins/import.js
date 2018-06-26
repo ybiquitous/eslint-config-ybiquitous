b943b19ace8c76c523a1b7f5bf4855f4881953bb
Masafumi Koba <ybiquitous@gmail.com>
2018-06-26 14:27:31 +0900

    chore(deps): update dependency eslint-plugin-import to v2.13.0 (#235)

--- rules/plugins/import.js
+++ rules/plugins/import.js
@@ -49,6 +49,7 @@ module.exports = {
     "import/no-named-default": "error",
     "import/no-namespace": "error",
     "import/no-nodejs-modules": "off",
+    "import/no-relative-parent-imports": "off",
     "import/no-restricted-paths": "off",
     "import/no-self-import": "error",
     "import/no-unassigned-import": "off",
