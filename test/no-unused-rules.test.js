3246efccc32ceab2e4fab0ffba6e5190ec38e6e0
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2019-08-20 23:56:48 +0900

    feat(typescript): list all TypeScript rules (#435)

--- test/no-unused-rules.test.js
+++ test/no-unused-rules.test.js
@@ -3,12 +3,6 @@ const { $, lintConfigFiles } = require("./helper");
 
 test("no unused rules", t => {
   lintConfigFiles.forEach(file => {
-    // FIXME: `Error: Cannot find module 'eslint-plugin-@typescript-eslint'`
-    if (file === "typescript.js") {
-      console.warn(`Skip '${file}' to avoid an error.`); // eslint-disable-line no-console
-      return;
-    }
-
     $("eslint-find-rules", "--unused", file);
     t.pass(file);
   });
