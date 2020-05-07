26c9f602554df6f98f4916a2fbd25c91a4178077
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-05-08 01:23:30 +0900

    feat(node): remove deprecated rules

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
     "./rules/core/variables.js",
     "./rules/core/styles.js",
     "./rules/core/es6.js",
-    "./rules/core/node-disable-all.js",
     "./rules/plugins/prettier.js",
     "./rules/plugins/eslint-comments.js",
     "./rules/plugins/import.js",
--- rules/core/node-disable-all.js
+++
@@ -1,8 +0,0 @@
-const nodeRules = require("./node");
-
-module.exports = {
-  rules: Object.keys(nodeRules.rules).reduce(
-    (newRules, ruleId) => Object.assign(newRules, { [ruleId]: "off" }),
-    {}
-  ),
-};
