c5d83435f8f4afc80c5c7fb8dad7ee80917dcf6a
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-05-26 18:29:16 +0900

    fix: turn off `eslint-comments/require-description`

--- rules/plugins/eslint-comments.js
+++ rules/plugins/eslint-comments.js
@@ -4,5 +4,6 @@ module.exports = {
     "eslint-comments/no-restricted-disable": "off",
     "eslint-comments/no-unused-disable": "error",
     "eslint-comments/no-use": "off",
+    "eslint-comments/require-description": "off",
   },
 };
