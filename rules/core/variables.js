fd1797138464278ef6a9f61b8e1ba7d6395fc831
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-07-13 00:27:57 +0900

    feat(core): remove `eslint-restricted-globals` package (#694)
    BREAKING CHANGE: The `no-restricted-globals` rule will warn fewer global variables.

--- rules/core/variables.js
+++ rules/core/variables.js
@@ -1,11 +1,18 @@
-const restrictedGlobals = require("eslint-restricted-globals");
-
 module.exports = {
   rules: {
     "init-declarations": "off",
     "no-delete-var": "error",
     "no-label-var": "error",
-    "no-restricted-globals": ["error", "isFinite", "isNaN", "alert"].concat(restrictedGlobals),
+    "no-restricted-globals": [
+      "error",
+      /* eslint-disable sort-keys */
+      { name: "isFinite", message: "Use `Number.isFinite` instead." },
+      { name: "isNaN", message: "Use `Number.isNaN` instead." },
+      { name: "alert", message: "Use `window.alert` instead." },
+      { name: "confirm", message: "Use `window.confirm` instead." },
+      { name: "location", message: "Use `window.location` instead." },
+      /* eslint-enable sort-keys */
+    ],
     "no-shadow": "error",
     "no-shadow-restricted-names": "error",
     "no-undef": "error",
