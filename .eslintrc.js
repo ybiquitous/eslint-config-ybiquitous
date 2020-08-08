73a7d23f2265c7fdb4b1aa118c0a71e43605b500
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-08 18:13:59 +0900

    test: tape -> jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,21 @@
 module.exports = {
   root: true,
+
   extends: ["./node.js"],
+
   overrides: [
     {
       files: ["rules/**/*.js"],
       rules: {
         "sort-keys": "error",
+      },
+    },
+    {
+      files: ["**/test/**/*.js"],
+      extends: ["plugin:jest/recommended"],
+      rules: {
+        "max-lines-per-function": "off",
+        "max-statements": "off",
       },
     },
   ],
