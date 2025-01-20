b6dc1cdd79b01c9fb96e5f4e1490dd153421f2b5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-21 00:46:44 +0900

    feat!: add `eslint.config.js` and remove `--ext` flag

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const node = require('./node.js');
+
+module.exports = [
+  {
+    files: ["**/*.{js,cjs,mjs,ts,cts,mts,jsx,tsx}"],
+
+    languageOptions: {
+      // TODO: Remove this configuration after the migration to ESM will complete.
+      globals: {
+        module: "readonly",
+        require: "readonly",
+      },
+    },
+
+    linterOptions: {
+      reportUnusedDisableDirectives: "warn",
+    },
+  },
+
+  ...node,
+
+  {
+    files: ["**/*.js"],
+    rules: {
+      "import/no-internal-modules": "off"
+    }
+  },
+
+  {
+    files: ["rules/**/*.js"],
+    rules: {
+      "sort-keys": "error",
+    },
+  },
+
+  // Must be at the last.
+  {
+    ignores: ["coverage", "dist", "tmp"],
+  },
+];
