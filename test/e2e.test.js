4acb9b62bc4ea26427a97607daf12f09d73a93ae
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-21 00:38:32 +0900

    style: fix format

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -2,7 +2,7 @@ import { writeFileSync, existsSync, unlinkSync } from "node:fs";
 import { EOL } from "node:os";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { test, expect, beforeAll, afterAll } from "vitest"; // eslint-disable-line import/named -- False positive.
+import { test, expect, beforeAll, afterAll } from "vitest";
 import pkg from "../package.json";
 import { sandbox, $ } from "./helper.js";
 
