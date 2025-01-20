d21fe968b586423e099d23dde0ddaa3b348e59b2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-21 00:48:54 +0900

    feat!: migrate `index.js`

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-module.exports = {
-  extends: [
-    "./rules/core.js",
-    "./rules/plugins/eslint-comments.js",
-    "./rules/plugins/import.js",
-    "./rules/plugins/regexp.js",
-    "./rules/plugins/prettier.js", // must be last
-  ],
-};
+const core = require("./rules/core.js");
+const eslintComments = require("./rules/plugins/eslint-comments.js");
+const importPlugin = require("./rules/plugins/import.js");
+const prettier = require("./rules/plugins/prettier.js");
+const regexp = require("./rules/plugins/regexp.js");
+
+module.exports = [
+  ...core,
+  ...eslintComments,
+  ...importPlugin,
+  ...regexp,
+  ...prettier, // must be last
+
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+    },
+  },
+];
