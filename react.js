14b9dcaf2b3f4e50a25753393507026c50d10e83
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-21 01:05:09 +0900

    feat!: migrate `react.js`

--- react.js
+++ react.js
@@ -1,10 +1,16 @@
-module.exports = {
-  extends: [
-    "./browser.js",
-    "./rules/plugins/react.js",
-    "./rules/plugins/react-jsx.js",
-    "./rules/plugins/react-hooks.js",
-    "./rules/plugins/jsx-a11y.js",
-    "plugin:import/react",
-  ],
-};
+const importPlugin = require("eslint-plugin-import");
+
+const browser = require("./browser.js");
+const jsxA11y = require("./rules/plugins/jsx-a11y.js");
+const reactHooks = require("./rules/plugins/react-hooks.js");
+const reactJsx = require("./rules/plugins/react-jsx.js");
+const react = require("./rules/plugins/react.js");
+
+module.exports = [
+  ...browser,
+  ...react,
+  ...reactHooks,
+  ...reactJsx,
+  ...jsxA11y,
+  importPlugin.flatConfigs.react,
+];
